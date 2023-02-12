965a7cb7540bb05b7f9763074cc1c075c19a483b
kazila <mrkazzila@gmail.com>
2023-02-12 19:28:20 +0300

    feat: create view for blog

--- portfolio/blog/views.py
+++ portfolio/blog/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+
 from common.views import TitleMixin
+from .models import BlogModel
 
 
-class BlogView(TitleMixin, TemplateView):
-    title = 'Portfolio'
+class BlogView(TitleMixin, ListView):
+    title = 'Blog'
     template_name = 'blog/all_blogs.html'
+    model = BlogModel
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['blogs'] = self.model.objects.all()
+        return context
