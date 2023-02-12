a23736c32648f0810559c9118a939d1b90a43bff
kazila <mrkazzila@gmail.com>
2023-02-12 15:35:27 +0300

    feat: add view for home page

--- portfolio/my_portfolio/views.py
+++ portfolio/my_portfolio/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from common.views import TitleMixin
+from .models import Project
 
-# Create your views here.
+
+class HomeView(TitleMixin, ListView):
+    title = 'Portfolio'
+    template_name = 'my_portfolio/portfolio.html'
+    model = Project
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['projects'] = self.model.objects.all()
+        return context
