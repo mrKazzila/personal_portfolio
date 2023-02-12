383d374afc4d424cc60aeec8839d522e3f938175
kazila <mrkazzila@gmail.com>
2023-02-12 14:49:21 +0300

    feat: add setting for media files

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -127,3 +127,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Media
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
