54f2cc8e4c81bda7ad79b36ae4e6bc8c65d64f30
kazila <mrkazzila@gmail.com>
2023-02-13 21:38:39 +0300

    feat: add str method

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -5,3 +5,6 @@ class BlogModel(models.Model):
     title = models.CharField(max_length=150)
     description = models.TextField(max_length=300)
     date_creation = models.DateField()
+
+    def __str__(self):
+        return self.title
