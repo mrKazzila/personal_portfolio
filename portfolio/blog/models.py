fd79c119060d0490f63b0c4745cddda84432644f
kazila <mrkazzila@gmail.com>
2023-02-21 23:22:54 +0300

    refactor: minor refactor

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 
 class BlogModel(models.Model):
-    title = models.CharField(max_length=150)
-    description = models.TextField(max_length=300)
+    title = models.CharField(
+        max_length=150,
+    )
+    description = models.TextField(
+        max_length=300,
+    )
     date_creation = models.DateField()
 
     def __str__(self):
