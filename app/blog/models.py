a9c4d6532957dcf50f0907d3faa0d104ee738a3d
mrKazzila <mrkazzila@gmail.com>
2023-06-04 23:46:26 +0300

    refactor!: move files from root to app dir & change main folder name (now config)

---
+++ app/blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class BlogModel(models.Model):
+    """Blog"""
+
+    title = models.CharField(max_length=150)
+    description = models.TextField(max_length=300)
+    date_creation = models.DateField()
+
+    def __str__(self):
+        return self.title
